3dd77a77f3f76397942ade90e84c6ef845b0d606
Jacinta Calixte <jacinta246@yahoo.com>
2020-06-14 19:34:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,12 +9,8 @@ end
 def monopoly_with_second_tier
   hash = Hash.new
   hash = {
-    :railroads => {
-      {one_piece_owned: "25"},
-      {two_pieces_owned: "50"},
-      {three_pieces_owned: "100"},
-      {four_pieces_owned: "200"}
-    }
+    :railroads => {},
+    
 
 
 end
